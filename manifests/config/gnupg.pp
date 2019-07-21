df4d5ce2c6aea2fa472bd142622e35f78516645b
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2019-07-21 16:58:39 +0300

    Get resource defaults from hash
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- manifests/config/gnupg.pp
+++ manifests/config/gnupg.pp
@@ -5,25 +5,26 @@
 #
 class duplicity::config::gnupg
 (
-    $ensure,
-    $gpg_key_id
+    Enum['present','absent'] $ensure,
+    String                   $gpg_key_id
 )
 {
-    validate_string($gpg_key_id)
 
-    Gnupg_key {
-        ensure => $ensure,
-        user   => 'root',
-        key_id => $gpg_key_id,
+    $gnupg_defaults = {
+        'ensure' => $ensure,
+        'user'   => 'root',
+        'key_id' => $gpg_key_id,
     }
 
     gnupg_key { 'duplicity-public-key':
         key_source => "puppet:///files/${gpg_key_id}-public.key",
         key_type   => 'public',
+        *          => $gnupg_defaults,
     }
 
     gnupg_key { 'duplicity-private-key':
         key_source => "puppet:///files/${gpg_key_id}-private.key",
         key_type   => 'private',
+        *          => $gnupg_defaults,
     }
 }
